aa9b62c88a78d9a3521bacf141e0c007642a1aa7
shpilson <kovalenko.jora@gmail.com>
2021-10-24 17:27:20 +0300

    update

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-let theme = document.querySelector('.theme');
-let themeButton = document.querySelector('.btn');
-
-themeButton.onclick = function() {
-    theme.classList.toggle('dark');
-    theme.classList.toggle('darkblue');
-    }
