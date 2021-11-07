ff2518094bf24ea6568f2538be2710f10e46fbd4
shpilson <kovalenko.jora@gmail.com>
2021-11-07 18:36:37 +0300

    update

--- js/themeSwitcher.js
+++ js/themeSwitcher.js
@@ -2,7 +2,8 @@ function themeSwitcher() {
     var wrapper = document.querySelector(".wrapper");
     wrapper.classList.toggle("dark");
 
-
+    var introduce = document.querySelector(".header__introduce-text");
+    introduce.classList.toggle("white");
 
     var switcher = document.getElementById("switcher");
     if (wrapper = document.querySelector(".dark")) {
