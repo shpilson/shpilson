284d1ca496fb6d6492f3ee64576363dd07289c21
shpilson <kovalenko.jora@gmail.com>
2021-11-07 18:34:27 +0300

    update

--- js/themeSwitcher.js
+++ js/themeSwitcher.js
@@ -1,8 +1,11 @@
 function themeSwitcher() {
-    var element = document.querySelector(".wrapper");
-    element.classList.toggle("dark");
+    var wrapper = document.querySelector(".wrapper");
+    wrapper.classList.toggle("dark");
+
+
+
     var switcher = document.getElementById("switcher");
-    if (element = document.querySelector(".dark")) {
+    if (wrapper = document.querySelector(".dark")) {
         switcher.src = "https://shpilson.github.io/shpilson/img/moon.svg";
     } else {
         switcher.src = "https://shpilson.github.io/shpilson/img/day.svg";
