233bb720bf13036674102abefaf0ab287d9dcb28
shpilson <kovalenko.jora@gmail.com>
2021-11-10 17:17:59 +0300

    update

--- js/themeSwitcher.js
+++ js/themeSwitcher.js
@@ -2,16 +2,19 @@ function themeSwitcher() {
     var wrapper = document.querySelector(".wrapper");
     wrapper.classList.toggle("dark-wrapper");
 
-    var introduce = document.querySelector(".header__introduce-text");
-    introduce.classList.toggle("dark-introduce");
-
-    var menu = document.querySelector(".header__menu-link");
-    menu.classList.toggle("dark-menu");
-
     var switcher = document.getElementById("switcher");
     if (wrapper = document.querySelector(".dark-wrapper")) {
         switcher.src = "https://shpilson.github.io/shpilson/img/moon.svg";
     } else {
         switcher.src = "https://shpilson.github.io/shpilson/img/day.svg";
     }
+
+    var introduce = document.querySelector(".header__introduce-text");
+    introduce.classList.toggle("dark-introduce");
+
+    var menu = document.querySelector(".header__menu-link");
+    menu.classList.toggle("dark-menu");
+
 }
+
+
