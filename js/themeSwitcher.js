fc314188bcec3c5316cf34d5d9795a351a37ec96
shpilson <kovalenko.jora@gmail.com>
2021-11-10 17:38:16 +0300

    update

--- js/themeSwitcher.js
+++ js/themeSwitcher.js
@@ -2,21 +2,20 @@ function themeSwitcher() {
     var wrapper = document.querySelector(".wrapper");
     wrapper.classList.toggle("dark-wrapper");
 
-    var introduce = document.querySelector(".header__introduce-text");
-    introduce.classList.toggle("dark-introduce");
-
-    var menu = document.querySelector(".header__menu-link");
-    menu.classList.toggle("dark-menu");
-
-    var works = document.querySelector(".works");
-    works.classList.toggle("dark-works");
-
-    var title = document.querySelector(".works");
-    title.classList.toggle("dark-title");
-
     var switcher = document.getElementById("switcher");
     if (wrapper = document.querySelector(".dark-wrapper")) {
         switcher.src = "https://shpilson.github.io/shpilson/img/moon.svg";
+        var introduce = document.querySelector(".header__introduce-text");
+        introduce.classList.toggle("dark-introduce");
+    
+        var menu = document.querySelector(".header__menu-link");
+        menu.classList.toggle("dark-menu");
+    
+        var works = document.querySelector(".works");
+        works.classList.toggle("dark-works");
+    
+        var title = document.querySelector(".works");
+        title.classList.toggle("dark-title");
     } else {
         switcher.src = "https://shpilson.github.io/shpilson/img/day.svg";
     }
