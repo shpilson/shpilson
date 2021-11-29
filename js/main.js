62bcce797eb21240167deb0eb5362d7a7359328e
shpilson <kovalenko.jora@gmail.com>
2021-11-29 15:58:43 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // Constants / Константы
-import pages from './config/conditions';
+import pages from './config/conditions.js';
 
 // Language switcher / Переключение языков
-import setLang from './langSwitcher'
+import setLang from './langSwitcher.js'
 
 if (pages.all) {
     setLang();
